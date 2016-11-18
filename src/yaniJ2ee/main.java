05825ccdf0a433f69d993f97081ba60fe758de33
yaniJ2ee <dislek0817@gmail.com>
2016-11-18 17:40:59 +0900

    git commit

---
+++ src/yaniJ2ee/main.java
@@ -0,0 +1,10 @@
+package yaniJ2ee;
+
+/**
+ * Created by yhlee on 2016-11-18.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.print("Git Test!!");
+    }
+}
